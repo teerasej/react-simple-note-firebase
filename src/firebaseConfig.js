e94a59f497afbbc12b573afb2f00b02e003e7f8a
Teerasej Jiraphatchandej <teerasej@gmail.com>
2021-12-08 20:32:50 +0700

    init firebase instance. config copied from firebase console

---
+++ src/firebaseConfig.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAPov2IbRYu06oi7fNAGUZ_bJIo8lAseWo",
+  authDomain: "ionicpwa-d8c79.firebaseapp.com",
+  databaseURL: "https://ionicpwa-d8c79.firebaseio.com",
+  projectId: "ionicpwa-d8c79",
+  storageBucket: "ionicpwa-d8c79.appspot.com",
+  messagingSenderId: "236296839382",
+  appId: "1:236296839382:web:935031e7f83afdf3f8e977"
+};
+
+// Initialize Firebase
+export default initializeApp(firebaseConfig);
