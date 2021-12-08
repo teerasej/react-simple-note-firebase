12a76fa03f1ddaeaea637522dbadb2d39d29a47c
Teerasej Jiraphatchandej <teerasej@gmail.com>
2021-12-08 21:32:06 +0700

    Let getAuth load token from cache Then render ui according to user signin state

--- src/App.js
+++ src/App.js
@@ -4,22 +4,35 @@ import { getAuth } from "firebase/auth";
 import firebaseApp from "./firebaseConfig"
 import SignInView from './components/SignInView';
 import { useSelector } from 'react-redux';
+import { useState } from 'react';
+import SignOutButton from './components/SignOutButton';
 
 
 function App() {
 
+
   const userSignedIn = useSelector(state => state.userSignedIn)
-  const auth = getAuth()
+  const [userFromAuth, setUserFromAuth] = useState(null)
   
+  getAuth().onAuthStateChanged(user => {
+    console.log('user from auth')
+    setUserFromAuth(user)
+  })
+
   return (
     <div>
       {
-        auth.currentUser && userSignedIn ? 
-          <div>Hello</div>
+        userFromAuth ? 
+          <div>
+            <div>Hello, {userFromAuth.email}</div>
+            <SignOutButton/>
+          </div>
           : <SignInView/>
       }
     </div>
   );
+  
+  
 }
 
 export default App;
