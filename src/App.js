f89e61aa49a66c3825b0163a1551264f62ac3b0a
Teerasej Jiraphatchandej <teerasej@gmail.com>
2021-12-08 20:33:00 +0700

    determine user loggin or not

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { getAuth } from "firebase/auth";
+import firebaseApp from "./firebaseConfig"
+
 
 function App() {
+
+  const auth = getAuth()
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {
+        auth.currentUser ? 
+          <div>Hello</div>
+          : <div>Please sign in</div>
+      }
     </div>
   );
 }
