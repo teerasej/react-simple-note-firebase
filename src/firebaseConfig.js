9bee07870cef469c0b0de0e6c941f30d7515fd6d
Teerasej Jiraphatchandej <teerasej@gmail.com>
2021-12-12 18:00:21 +0700

    update for using in practice

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -5,13 +5,7 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAPov2IbRYu06oi7fNAGUZ_bJIo8lAseWo",
-  authDomain: "ionicpwa-d8c79.firebaseapp.com",
-  databaseURL: "https://ionicpwa-d8c79.firebaseio.com",
-  projectId: "ionicpwa-d8c79",
-  storageBucket: "ionicpwa-d8c79.appspot.com",
-  messagingSenderId: "236296839382",
-  appId: "1:236296839382:web:935031e7f83afdf3f8e977"
+ //...
 };
 
 // Initialize Firebase
